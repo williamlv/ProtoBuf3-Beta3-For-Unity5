a5d8b66b6a6e78b220b8c1477d84dbd7fde5ef06
wlv <hamenny@gmail.com>
2016-05-21 02:20:03 +0800

    test read

---
+++ Assets/TestProto.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Sapphire;
+using Google.Protobuf;
+using System.IO;
+
+public class TestProto : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        login msg = new login
+        {
+            Username = "abcdef",
+            Passwd = "123456"
+        };
+        using (var output = File.Create("test.dat"))
+        {
+            msg.WriteTo(output);
+        }
+        //        byte[] buffer = new byte[msg.CalculateSize()];
+
+        //CodedOutputStream stream = new CodedOutputStream(new byte[msg.CalculateSize()]);
+        //msg.WriteTo(stream);
+        login read;
+        using (var input = File.OpenRead("test.dat"))
+        {
+            read = login.Parser.ParseFrom(input);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
